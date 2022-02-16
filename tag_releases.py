2b25c3f4223b402d153dc187087bd20e705d347f
Suchandra Thapa <suchandra@gmail.com>
2022-02-16 12:41:01 -1000

    Add tests and tagging implementation to tag_releases.py

--- tag_releases.py
+++ tag_releases.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-
+import json
 import sys
 import pathlib
 import time
 from dataclasses import dataclass
 
 import requests
+import rich
 from rich.console import Console
 from rich.table import Table
+from rich.progress import track
 import toml
 import click
 
-GITHUB_BASE_URL = "https://api.github.com/"
+GITHUB_BASE_URL = "https://api.github.com"
 GITHUB_ORGANIZATION = "ssl-hep"
 
 
@@ -21,20 +23,22 @@ class RepoInfo:
   branch: str
   label: str
   tagtype: str
-  semver: str
-  tag: str
+  semver: str = ""
+  tag: str = ""
 
 
-def error_exit(message: str = None) -> None:
+def error(message: str = None, quit: bool = True) -> None:
   """
   Print an optional error message and exit
 
   :param message: message to print
+  :param quit: if true, exit program
   :return: None
   """
   if not message:
     message = "Unknown fatal error occurred"
-    Console().print(message, style="bold red")
+  Console().print(message, style="bold red")
+  if quit:
     sys.exit(1)
 
 
@@ -47,35 +51,40 @@ def ingest_config(config_file: pathlib.Path) -> list[RepoInfo]:
   """
   parsed = toml.load(config_file)
   repos = []
-  for k,val in parsed:
-    if 'branch' in val:
-      branch = val['branch']
+  for key in parsed.keys():
+    if 'branch' in parsed[key]:
+      branch = parsed[key]['branch']
     else:
-      error_exit(f"Section {k} missing branch setting")
-    if 'label' in val:
-      label = val['label']
+      error(f"Section {key} missing branch setting")
+    if 'label' in parsed[key]:
+      label = parsed[key]['label']
     else:
-      error_exit(f"Section {k} missing label setting")
-    if 'tagtype' in val:
-      tagtype = val['tagtype'].lower()
-      if tagtype not in ['semvar', 'calver']:
-        error_exit(f"In section {k}, tagtype must be 'semver' or 'calver', got {tagtype}")
+      error(f"Section {key} missing label setting")
+    if 'tagtype' in parsed[key]:
+      tagtype = parsed[key]['tagtype'].lower()
+      if tagtype not in ['semver', 'calver']:
+        error(f"In section {key}, tagtype must be 'semver' or 'calver', got {tagtype}")
     else:
-      error_exit(f"Section {k} missing tagtype setting")
-    if 'semver' in val:
-      semver = val['semver']
+      error(f"Section {key} missing tagtype setting")
+    if 'semver' in parsed[key]:
+      semver = parsed[key]['semver']
     else:
       semver = ""
-    repos.append(RepoInfo(k, branch, label, tagtype, semver, ""))
+    repos.append(RepoInfo(key, branch, label, tagtype, semver))
   return repos
 
 
-def generate_calver() -> str:
+def generate_calver(date: time.struct_time = None) -> str:
   """
   Generate and return a calver (YYYYMMDD-HHMM)
+  :param date: optional date
+  :return: string with calver
   """
-  now = time.localtime()
-  return f"{now.tm_year:04}{now.tm_mon:02}{now.tm_mday:02}-{now.tm_hour}{now.tm_min}"
+  if date:
+    target_date = date
+  else:
+    target_date = time.localtime()
+  return f"{target_date.tm_year:04}{target_date.tm_mon:02}{target_date.tm_mday:02}-{target_date.tm_hour:02}{target_date.tm_min:02}"
 
 
 def generate_tag(repo: RepoInfo) -> str:
@@ -96,7 +105,7 @@ def get_confirmation(tagged_repos: list[RepoInfo]) -> bool:
   :return: true if user confirms, false otherwise
   """
   console = Console()
-  table = Table("Repo tags to be applied")
+  table = Table(title="Repo tags to be applied")
   table.add_column("Repository")
   table.add_column("URL")
   table.add_column("Tag")
@@ -120,32 +129,34 @@ def generate_repo_url(repo_config: RepoInfo) -> str:
   return f"{GITHUB_BASE_URL}/repos/{GITHUB_ORGANIZATION}/{repo_config.name}"
 
 
-def verify_branch(repo_config: RepoInfo) -> bool:
+def verify_branch(repo_config: RepoInfo, github_token: str = None) -> bool:
   """
   Check and verify a branch exists in a repository
   :param repo_config:  repo config
+  :param github_token: github token to use for authentication
   :return: True if branch is present
   """
-  branch_url = generate_repo_url(repo_config) + "/branches"
-  r = requests.get(branch_url)
-  if r.status_code != 200:
-    error_exit(f"Got {r.status_code} when accessing github: {branch_url}")
-  resp_json = r.json()
-  for branch in resp_json:
-    if branch['name'] == repo_config.branch:
-      return True
+  branch_url = generate_repo_url(repo_config) + f"/branches/{repo_config.branch}"
+  r = requests.get(branch_url, headers={'Accept': 'application/vnd.github.v3+json'})
+  match r.status_code:
+    case 404: return False
+    case 200: return True
+    case _: error(f"Got {r.status_code} when accessing github: {branch_url}")
   return False
 
 
-def check_repos(repo_configs: list[RepoInfo]) -> None:
+def check_repos(repo_configs: list[RepoInfo], github_token: str = None) -> bool:
   """
   Check a list of repos to verify that information provided is accurate
   :param repo_configs: list of RepoInfo to check
+  :param github_token: github token to use for authentication
   :return: None
   """
   for repo in repo_configs:
-    if not verify_branch(repo):
-      error_exit(f"Can't verify that {repo.branch} exists in {repo.name}")
+    if not verify_branch(repo, github_token):
+      error(f"Can't verify that {repo.branch} exists in {repo.name}", quit=False)
+      return False
+  return True
 
 
 def generate_repo_tags(repo_configs: list[RepoInfo]) -> list[RepoInfo]:
@@ -163,18 +174,67 @@ def generate_repo_tags(repo_configs: list[RepoInfo]) -> list[RepoInfo]:
   return new_configs
 
 
-def tag_repos(repo_configs: list[RepoInfo]) -> None:
+def tag_repos(repo_configs: list[RepoInfo], github_token = None) -> None:
   """
   Tag repos with specified tags
   :param repo_configs: list of repo configurations
   :return:  None
   """
-  check_repos(repo_configs)
-  tagged_configs = generate_tag(repo_configs)
+  check_repos(repo_configs, github_token)
+  tagged_configs = generate_repo_tags(repo_configs)
   if not get_confirmation(tagged_configs):
-    error_exit("Tagging operation was not confirmed")
-  for repo in tagged_configs:
-    pass
+    error("Tagging operation was not confirmed", quit=True)
+  Console().print("Starting tagging operations:")
+  for repo in track(tagged_configs, description="Tagging.."):
+    if not tag_repo(repo, github_token):
+      error(f"Can't tag {repo.name}")
+
+
+def tag_repo(repo_config: RepoInfo, github_token: str = None) -> bool:
+  """
+  Tag a repo branch
+  :param repo_config: configuration for repo
+  :param github_token: github token for authentication
+  :return: None
+  """
+
+  if not github_token or not github_token.startswith('ghp') :
+    error("Must provide a valid github token for authentication", quit=False)
+    print(github_token)
+    return False
+
+  branch_url = generate_repo_url(repo_config) + f"/branches/{repo_config.branch}"
+  r = requests.get(branch_url,
+                   headers={'Accept': 'application/vnd.github.v3+json',
+                            'Authorization': f"token {github_token}"})
+  match r.status_code:
+    case 200: json_body = r.json()
+    case _: return False
+  commit = json_body['commit']['sha']
+
+  r = requests.post(generate_repo_url(repo_config) + "/git/tags",
+                    data=json.dumps({"owner": "ssl-hep",
+                                     "repo": repo_config.name,
+                                     "tag": repo_config.tag,
+                                     "message": "Tagged using tag_releases.py",
+                                     "object": commit,
+                                     "type": "commit"}),
+                    headers={'Accept': 'application/vnd.github.v3+json',
+                             'Authorization': f"token {github_token}"})
+  if r.status_code != 201:
+    error(f"Error while creating a tag for a {repo_config.name} commit: {commit}: {r.json()}")
+    return False
+  resp = r.json()
+  tag_sha = resp["sha"]
+  r = requests.post(generate_repo_url(repo_config) + "/git/refs",
+                    data=json.dumps({"ref": f"refs/tags/{repo_config.tag}",
+                                     "sha": tag_sha}),
+                    headers={'Accept': 'application/vnd.github.v3+json',
+                             'Authorization': f"token {github_token}"})
+  if r.status_code != 201:
+    error(f"Error while creating a ref for a {repo_config.name} commit: {tag_sha}: {r.json()}")
+    return False
+  return True
 
 
 @click.command()
@@ -187,13 +247,17 @@ def entry(config: str) -> None:
   """
   console = Console()
   config_path = pathlib.Path(config)
+  token = input("Enter your github token (PAT):")
+  if token == "" or not token.startswith('ghp'):
+    console.print("Token seems to be invalid, continuing without one", style="yellow")
+    token = None
   if pathlib.Path.is_file(config_path):
     repo_configs = ingest_config(config_path)
   else:
-    console.print(f"Config file {config} not present\n", style="bold red")
-    sys.exit(1)
-  check_repos(repo_configs)
-  # tag_repos(repo_config)
+    error(f"Config file {config} not present\n")
+  if not check_repos(repo_configs, token):
+    error(f"Can't verify that all repos and branches exist")
+  tag_repos(repo_configs, token)
   sys.exit(0)
 
 
---
+++ tests/test_tag_releases.py
@@ -0,0 +1,71 @@
+import pathlib
+import time
+
+import tag_releases
+
+class TestTagRelease:
+    """
+    Class to test tag_releases.py code
+    """
+
+    def test_ingest_config(self):
+        """
+        Test config ingestion
+        :return: None
+        """
+        expected_config = [tag_releases.RepoInfo("test1", "develop", "develop1", "calver"),
+                           tag_releases.RepoInfo("test2", "release", "release1", "semver", "1.2.4rc2")]
+
+        config = tag_releases.ingest_config(pathlib.Path("test.toml"))
+        assert(config == expected_config)
+
+    def test_generate_repo_url(self):
+        """
+        Test generate repo url code
+        :return: None
+        """
+        config = tag_releases.ingest_config(pathlib.Path("test.toml"))
+        assert(tag_releases.generate_repo_url(config[0]) == "https://api.github.com/repos/ssl-hep/test1")
+        assert(tag_releases.generate_repo_url(config[1]) == "https://api.github.com/repos/ssl-hep/test2")
+
+    def test_generate_calver(self):
+        """
+        Test calver generation
+        :return: None
+        """
+        test_time = time.strptime("2022-02-16 09:18", "%Y-%m-%d %H:%M")
+        assert(tag_releases.generate_calver(test_time) == "20220216-0918")
+
+    def test_generate_tag(self):
+        """
+        Test tag generation
+        :return: None
+        """
+        config = tag_releases.ingest_config(pathlib.Path("test.toml"))
+        now = time.localtime()
+        calver = f"{now.tm_year:04}{now.tm_mon:02}{now.tm_mday:02}-{now.tm_hour:02}{now.tm_min:02}"
+
+        assert(tag_releases.generate_tag(config[0]) == f"{calver}-develop1")
+        assert (tag_releases.generate_tag(config[1]) == f"1.2.4rc2-release1")
+
+    def test_verify_branch(self):
+        """
+        Test functionality to verify that a repo exists with the appropriate branch
+        :return: None
+        """
+        config = tag_releases.ingest_config(pathlib.Path("test.toml"))
+        assert(tag_releases.verify_branch(config[0]))
+        assert(tag_releases.verify_branch(config[1]))
+        config = tag_releases.ingest_config(pathlib.Path("invalid.toml"))
+        assert(not tag_releases.verify_branch(config[0]))
+        assert(tag_releases.verify_branch(config[1]))
+
+    def test_check_repos(self):
+        """
+        Test functionality to verify that a repo exists
+        :return: None
+        """
+        config = tag_releases.ingest_config(pathlib.Path("test.toml"))
+        assert(tag_releases.check_repos(config))
+        config = tag_releases.ingest_config(pathlib.Path("invalid.toml"))
+        assert (not tag_releases.check_repos(config))
